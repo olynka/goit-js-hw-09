27a033faee84f45e79939ba839c5f9881909409f
olynka <98587532+olynka@users.noreply.github.com>
2022-08-21 17:29:30 +0300

    correction of the second problem

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,6 @@
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from "notiflix";
-
 refs = {
     inputEl: document.querySelector('#datetime-picker'),
     BtnEl: document.querySelector('button[data-start]'),
@@ -12,14 +10,13 @@ refs = {
     dataSecondsEl: document.querySelector('[data-seconds]')
 };
 let interval = null;
-
+let currentTime = null;
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
@@ -28,16 +25,11 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
-
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -50,26 +42,21 @@ const options = {
             return;
         }
         refs.BtnEl.removeAttribute('disabled');
+        currentTime = selectedDates[0].getTime();
+        console.log(currentTime);
     },
 };
-
 function addLeadingZero(value) {
     return String(value).padStart(2, 0);
 };
-
-
-
-
+flatpickr('#datetime-picker', options);
 refs.BtnEl.addEventListener('click', handleStartData);
-
-
 function handleStartData() {
     refs.BtnEl.setAttribute('disabled', 'true');
     refs.inputEl.setAttribute('disabled', 'true');
     const date = new Date(refs.inputEl.value).getTime();
     interval = setInterval(() => {
         const { days, hours, minutes, seconds } = convertMs(date - Date.now());
-
         refs.dataDaysEl.textContent = addLeadingZero(days);
         refs.dataHoursEl.textContent = addLeadingZero(hours);
         refs.dataMinutesEl.textContent = addLeadingZero(minutes);
@@ -82,4 +69,3 @@ function handleStartData() {
         }
     }, 1000);
 };
-flatpickr('#datetime-picker', options);
